f1ef273ee0960931189cfda00033c2dab3980b92
Darío Candotti <dcandotti@siu.edu.ar>
2024-02-11 23:52:27 -0300

    Se agrega la interfaz de jurisdicción

---
+++ src/Jurisdiccion.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sisfet;
+
+interface Jurisdiccion
+{
+	function matches (string $nombreAproximado): bool;
+	function revisar (Tramite $tramite, Usuario $usuario): void;
+	function apruebaSupervisor (Tramite $tramite, Usuario $usuario, string $observaciones): void;
+	function apruebaAreaCentral (Tramite $tramite, Usuario $usuario, string $observaciones): void;
+	function apruebaFirmante (Tramite $tramite, Usuario $usuario, string $observaciones): void;
+	function enviarSinFirma (contenido_analitico $analitico): void;
+}
